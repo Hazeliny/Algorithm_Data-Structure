78175f8c4228b040a9e2a0ea60ad1a3cd052ba28
Lin Yao <linyao@car5s5.42barcelona.com>
2024-10-23 18:01:09 +0200

    linkedlist begin

---
+++ C++/arrayLinkedlist/linkedList.cpp
@@ -0,0 +1,34 @@
+/*
+struct listNode {
+    int val;
+    listNode *next;
+    listNode(int x) : val(x), next(nullptr) {} // constructor
+    };
+
+listNode* n0 = new listNode(1);
+listNode* n1 = new listNode(3);
+listNode* n2 = new listNode(2);
+listNode* n3 = new listNode(5);
+listNode* n4 = new listNode(4);
+n0->next = n1;
+n1->next = n2;
+n2->next = n3;
+n3->next = n4;
+
+// doubly linked list
+struct listNode {
+    int val;
+    listNode *next;
+    listNode *prev;
+    listNode(int x) : val(x), next(nullptr), prev(nullptr) {} // constructor
+    };
+*/
+
+int main() {
+
+    listNode* n0 = new listNode(1);
+    listNode* n1 = new listNode(3);
+    listNode* n2 = new listNode(2);
+    listNode* n3 = new listNode(5);
+    listNode* n4 = new listNode(4);
+}
